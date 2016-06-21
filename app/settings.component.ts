86fe5462144c83cd2b4674754c5de95f9925a95b
Lucas Cherkewski <lucas@ecustom.ca>
2016-06-21 19:35:45 -0400

    give that SettingsService some teeth

--- app/settings.component.ts
+++ app/settings.component.ts
@@ -1,9 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+
+import {SettingsService} from "./settings.service";
 
 @Component({
   selector: 'user-settings',
-  template: '<h2>Settings</h2>'
+  template: `
+    <h2>Settings</h2>
+    
+  `,
+  providers: [SettingsService]
 })
-export class SettingsComponent {
-  
+export class SettingsComponent implements OnInit {
+  settings: Object;
+
+  constructor(private settingsService: SettingsService) {}
+
+  getSettings() {
+    this.settingsService.getSettings().then(settings => this.settings = settings);
+  }
+
+  ngOnInit() {
+    this.getSettings();
+  }
 }
--- app/settings.service.ts
+++ app/settings.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 
 @Injectable()
-export class ShelfService {
+export class SettingsService {
   getSettings() {
     return Promise.resolve(localStorage.getItem('fww-settings'));
   }
