345747d047994603fd3634ac47a932db76d9f925
Lucas Cherkewski <lucas@ecustom.ca>
2016-06-21 17:30:32 -0400

    create Shelves mock data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core';
 
+export class Shelf {
+  id: number;
+  name: string;
+  book_count: number;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>{{title}}</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Fast Web Wrapper';
+  public shelves = SHELVES;
+}
+
+const SHELVES: Shelf[] = [
+  {
+    id: 87656268,
+    name: 'read',
+    book_count: 92
+  }
+];
