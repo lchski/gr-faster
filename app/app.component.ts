376b42c4fe28e6d777162b7f4a92dc4baf574e82
Lucas Cherkewski <lucas@ecustom.ca>
2016-06-21 17:32:02 -0400

    expand mock shelf list

--- app/app.component.ts
+++ app/app.component.ts
@@ -20,5 +20,15 @@ const SHELVES: Shelf[] = [
     id: 87656268,
     name: 'read',
     book_count: 92
+  },
+  {
+    id: 87656267,
+    name: 'currently-reading',
+    book_count: 8
+  },
+  {
+    id: 87656266,
+    name: 'to-read',
+    book_count: 570
   }
 ];
